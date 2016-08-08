78835a6239f077fc1c8c97eacefb11794c62108b
sigmundliu <sigmundliu@192.168.36.103>
2016-08-08 11:55:32 +0800

    Initial Commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by sigmundliu on 16/8/8.
+//  Copyright © 2016年 sigmundliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
